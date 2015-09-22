8a164cf34c7d4febef854116f533307b2b4e8aa8
Max Arnold <arnold.maxim@gmail.com>
2015-09-22 15:53:59 +0700

    misc fixes and formatting

--- slack/slack.php
+++ slack/slack.php
@@ -14,48 +14,51 @@ class SlackPlugin extends Plugin {
     function onTicketCreated($ticket){
         try {
             global $ost;
+
             $payload = array(
-                        'attachments' =>
+                'attachments' =>
+                array (
+                    array (
+                        'pretext' => "Создана новая заявка <" . $ost->getConfig()->getUrl() . "scp/tickets.php?id="
+                        . $ticket->getId() . "|#" . $ticket->getNumber() . "> в категории ". Format::htmlchars($ticket->getDept() instanceof Dept ? $ticket->getDept()->getName() : '')
+                        ." через " . $ticket->getSource() . " (" . Format::db_datetime($ticket->getCreateDate()) . " MSK)",
+                        'fallback' => "Создана новая заявка <" . $ost->getConfig()->getUrl() . "scp/tickets.php?id="
+                        . $ticket->getId() . "|#" . $ticket->getNumber() . "> в категории ". Format::htmlchars($ticket->getDept() instanceof Dept ? $ticket->getDept()->getName() : '')
+                        ." через " . $ticket->getSource() . " (" . Format::db_datetime($ticket->getCreateDate()) . " MSK)",
+                        'color' => "#D00000",
+                        'fields' =>
+                        array(
                             array (
-                                array (
-                                    'pretext' => "New Ticket <" . $ost->getConfig()->getUrl() . "scp/tickets.php?id="
-                                                . $ticket->getId() . "|#" . $ticket->getNumber() . "> created",
-                                    'fallback' => "New Ticket <" . $ost->getConfig()->getUrl() . "scp/tickets.php?id="
-                                                . $ticket->getId() . "|#" . $ticket->getNumber() . "> created",
-                                    'color' => "#D00000",
-                                    'fields' =>
-                                    array(
-                                        array (
-                                            'title' => $ticket->getSubject(),
-                                            'value' => "created by " . $ticket->getName() . "(" . $ticket->getEmail()
-                                                        . ") in " . $ticket->getDeptName() . "(Department) via "
-                                                        . $ticket->getSource(),
-                                            'short' => False,
-                                        ),
-                                    ),
-                                ),
+                                'title' => mb_convert_case(Format::htmlchars($ticket->getName()), MB_CASE_TITLE) . " (".$ticket->getEmail().")",
+                                'value' => $ticket->getSubject(), // $ticket->getLastMessage()->getBody()
+                                'short' => false
                             ),
-                        );
+                        ),
+                    ),
+                ),
+            );
 
             $data_string = utf8_encode(json_encode($payload));
             $url = $this->getConfig()->get('slack-webhook-url');
 
+            if (!function_exists('curl_init')){
+                error_log('osticket slackplugin error: cURL is not installed!');
+            }
+
             $ch = curl_init($url);
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-                'Content-Type: application/json',
-                'Content-Length: ' . strlen($data_string))
-            );
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            $result = curl_exec($ch);
 
-            if(curl_exec($ch) === false){
-                throw new Exception($url . ' - ' . curl_error($ch));
+            if($result === false){
+                error_log($url . ' - ' . curl_error($ch));
             }
             else{
                 $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
                 if($statusCode != '200'){
-                    throw new Exception($url . ' Http code: ' . $statusCode);
+                    error_log($url . ' Http code: ' . $statusCode);
                 }
             }
             curl_close($ch);
